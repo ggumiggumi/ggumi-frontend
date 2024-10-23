d865021f6c50d2d210d250c47315c77ed26a9f00
heogeonho <geonh1016@gmail.com>
2024-10-23 23:09:37 +0900

    feat: ProfileCard 컴포넌트 이미지 변환 로직 수정(public 폴더의 이미지 활용)

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,32 +1,15 @@
 import React from 'react';
 import '../styles/ProfileCard.css';
-import profile1 from '../assets/profile-code1.png';
-import profile2 from '../assets/profile-code2.png';
-import profile3 from '../assets/profile-code3.png';
 
 const ProfileCard = ({ profileImageCode, name, onSelect }) => {
-    let profileImage;
-
-    // profileImageCode에 따라 이미지 선택
-    switch (profileImageCode) {
-        case 1:
-            profileImage = profile1;
-            break;
-        case 2:
-            profileImage = profile2;
-            break;
-        case 3:
-            profileImage = profile3;
-            break;
-        default:
-            profileImage = profile1; // 기본 이미지 설정 (필요에 따라 변경)
-            break;
-    }
-
     return (
         <div className='profile-card' onClick={onSelect}>
-             <button className='profile-button'>
-                <img src={profileImage} alt={name} className='profile-image' />
+            <button className='profile-button'>
+                <img
+                    src={`/profile-code${profileImageCode}.png`}
+                    alt={name}
+                    className='profile-image'
+                />
             </button>
             <p>{name}</p>
         </div>
