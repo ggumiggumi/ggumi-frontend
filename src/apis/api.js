464d58a94f8b24dfd845e0ea5d4df42d32e66953
고경남 <rhrudska987@naver.com>
2024-10-24 13:24:11 +0900

    feat/kakao-socail-login

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,4 +1,4 @@
 export const LOCAL_DOMAIN = "http://localhost:8080";
-export const SERVER_DOMAIN = "http://13.125.110.207:8080";
+export const SERVER_DOMAIN = "https://hyodev.kr";
 
-export const API_DOMAIN = `${LOCAL_DOMAIN}/api`;
+export const API_DOMAIN = `${SERVER_DOMAIN}/api`;
